580b2f22f657bdf6980443c1f32f83a3021a9aba
JuanJUribe <juanjouribev1@gmail.com>
2020-11-04 08:54:49 -0800

    Add augmentation experiments 1-3

---
+++ experiments/sentence-aug/architecture/dataset_reader.py
@@ -0,0 +1,34 @@
+from typing import Iterator, List, Dict
+
+from allennlp.data import Instance
+from allennlp.data.dataset_readers import DatasetReader
+from allennlp.data.fields import TextField, LabelField
+from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
+from allennlp.data.tokenizers import Token, Tokenizer, WhitespaceTokenizer
+
+@DatasetReader.register('tsv-reader')
+class TSVDatasetReader(DatasetReader):
+    
+    def __init__(self, 
+                 tokenizer: Tokenizer = None,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.tokenizer = tokenizer or WhitespaceTokenizer()
+        self.token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
+
+    def text_to_instance(self, sent: str, label: str = None) -> Instance:
+        tokens = self.tokenizer.tokenize(sent)
+        text_field = TextField(tokens, self.token_indexers)
+        fields = {'text': text_field}
+
+        if label:
+            fields['label'] = LabelField(label)
+            
+        return Instance(fields)
+    
+    def _read(self, file_path: str) -> Iterator[Instance]:
+        with open(file_path, 'r', encoding='utf8') as lines:
+            for line in lines:
+                sent, label = line.replace('\n', '').split('\t')
+                yield self.text_to_instance(sent, label)
