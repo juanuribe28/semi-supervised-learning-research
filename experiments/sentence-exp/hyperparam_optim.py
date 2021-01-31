b47652326d614679ae50f06fc76ac170c8cd5b62
Juan-tensorboard <juanjouribev1@gmail.com>
2021-01-31 11:22:28 -0800

    Update hyperaram-optim files to run with each model

--- experiments/sentence-exp/hyperparam_optim.py
+++ experiments/sentence-exp/hyperparam_optim.py
@@ -1,6 +1,31 @@
 import optuna
 
 def objective(trial: optuna.Trial) -> float:
-    trial.suggest_int('embedding_dim', 16, 1024)
-    trial.suggest_float('lr', 5e-4, 5e-1, log=True)
+    trial.suggest_int('embedding_dim', 16, 512)
+    trial.suggest_float('dropout', 0.0, 0.9)
+    trial.suggest_float('lr', 5e-4, 5e-2, log=True)
     
+    executor = optuna.integration.allennlp.AllenNLPExecutor(
+        trial=trial,
+        config_file="./training_config/config.jsonnet",  # path to jsonnet
+        serialization_dir=f"./results/optuna/{trial.number}",
+        metrics="best_validation_accuracy"
+    )
+    return executor.run()
+
+if __name__ == '__main__':
+    study = optuna.create_study(
+        storage="sqlite:////home/juan/Research/research-f2020/experiments/hyperparam-optim.db",  # save results in DB
+        sampler=optuna.samplers.TPESampler(seed=24),
+        study_name="sentence-exp0",
+        direction="maximize",
+        pruner=optuna.pruners.HyperbandPruner()
+    )
+
+    timeout = 60 * 60 * 10  # timeout (sec): 60*60*10 sec => 10 hours
+    study.optimize(
+        objective,
+        n_jobs=1,  # number of processes in parallel execution
+        n_trials=100,  # number of trials to train a model
+        timeout=timeout,  # threshold for executing time (sec)
+    )
--- experiments/verb-sentence-exp/hyperparam_optim.py
+++ experiments/verb-sentence-exp/hyperparam_optim.py
@@ -5,21 +5,21 @@ def objective(trial: optuna.Trial) -> float:
     trial.suggest_int('v_embedding_dim', 16, 512)
     trial.suggest_float('s_dropout', 0.0, 0.9)
     trial.suggest_float('v_dropout', 0.0, 0.9)
-    trial.suggest_float('lr', 5e-3, 5e-1, log=True)
+    trial.suggest_float('lr', 5e-4, 5e-2, log=True)
     
     executor = optuna.integration.allennlp.AllenNLPExecutor(
         trial=trial,
         config_file="./training_config/config.jsonnet",  # path to jsonnet
-        serialization_dir=f"./results/optuna-test/{trial.number}",
+        serialization_dir=f"./results/optuna2/{trial.number}",
         metrics="best_validation_accuracy"
     )
     return executor.run()
 
 if __name__ == '__main__':
     study = optuna.create_study(
-        storage="sqlite:///results/test.db",  # save results in DB
+        storage="sqlite:////home/juan/Research/research-f2020/experiments/hyperparam-optim.db",  # save results in DB
         sampler=optuna.samplers.TPESampler(seed=24),
-        study_name="optuna_allennlp",
+        study_name="verb-sentence-exp2",
         direction="maximize",
         pruner=optuna.pruners.HyperbandPruner()
     )
