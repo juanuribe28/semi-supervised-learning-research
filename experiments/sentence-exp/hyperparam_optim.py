cbb25c87a1f1ccac51af603caa3b07dfc012ce84
Juan-tensorboard <juanjouribev1@gmail.com>
2021-04-07 22:38:41 -0700

    Update config for new data_aug runs

--- experiments/sentence-exp/hyperparam_optim.py
+++ experiments/sentence-exp/hyperparam_optim.py
@@ -3,12 +3,12 @@ import optuna
 def objective(trial: optuna.Trial) -> float:
     trial.suggest_int('embedding_dim', 16, 512)
     trial.suggest_float('dropout', 0.0, 0.9)
-    trial.suggest_float('lr', 5e-4, 5e-2, log=True)
+    trial.suggest_float('lr', 5e-5, 5e-3, log=True)
     
     executor = optuna.integration.allennlp.AllenNLPExecutor(
         trial=trial,
         config_file="./training_config/config.jsonnet",  # path to jsonnet
-        serialization_dir=f"./results/optuna/{trial.number}",
+        serialization_dir=f"./results/optuna_aug8/{trial.number}",
         metrics="best_validation_accuracy"
     )
     return executor.run()
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     study = optuna.create_study(
         storage="sqlite:////home/juan/Research/research-f2020/experiments/hyperparam-optim.db",  # save results in DB
         sampler=optuna.samplers.TPESampler(seed=24),
-        study_name="sentence-exp0",
+        study_name="sentence-exp_aug8",
         direction="maximize",
         pruner=optuna.pruners.HyperbandPruner()
     )
